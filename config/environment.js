7250c78d56de8641544856cc25218c05df860ee1
Jonan Scheffler <jonanscheffler@gmail.com>
2014-06-17 21:01:54 -0700

    Fix api url.
    Fix api url.

--- config/environment.js
+++ config/environment.js
@@ -27,7 +27,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.APP.API_URL = 'http://bridgeit.herokuapp.com'
+    ENV.APP.API_URL = 'http://bridgeitapi.herokuapp.com'
   }
 
   return ENV;
