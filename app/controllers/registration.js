2cb8ba75fb9fc558d4789fba9ea0bee3de8623dc
Jonan Scheffler <jonanscheffler@gmail.com>
2014-06-18 20:48:01 -0700

    Add thank you notification.
    Add thank you notification.

--- app/controllers/registration.js
+++ app/controllers/registration.js
@@ -3,6 +3,7 @@ export default Ember.ObjectController.extend({
     save: function(model){
       var self = this;
       model.save().then(function(){
+        $('div#notification').fadeIn(1000).fadeOut(1000);
         self.set('model', self.store.createRecord('guest'));
       });
     }
